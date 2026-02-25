0dc3e38e18cd7ac12e73efab87079a64a55cd053
Rebecca <rebforman@yahoo.com>
2026-02-25 13:51:17 -0500

    Completed mergeQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -143,5 +143,11 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number },
 ): Question {
-    return contentQuestion;
+    return {
+        ...contentQuestion,
+        id: id,
+        name: name,
+        points: points,
+        published: false,
+    };
 }
