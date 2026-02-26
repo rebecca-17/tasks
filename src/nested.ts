e47161e8a167dc6c790fbf46d9ce5c3663fa0b4b
Rebecca <rebforman@yahoo.com>
2026-02-26 09:22:59 -0500

    Completed makeAnswers

--- src/nested.ts
+++ src/nested.ts
@@ -97,7 +97,14 @@ export function toCSV(questions: Question[]): string {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-    return [];
+    return questions.map(
+        (ques: Question): Answer => ({
+            questionId: ques.id,
+            text: "",
+            submitted: false,
+            correct: false,
+        }),
+    );
 }
 
 /***
